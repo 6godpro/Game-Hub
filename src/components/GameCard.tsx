3c4ebce0905bbe34eb4d48ce306900b80b232602
6godpro <erickson5115@gmail.com>
2024-06-06 12:07:15 +0100

    fix bug for games without platform object

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -16,7 +16,7 @@ const GameCard = ({ game }: Props) => {
         <Heading fontSize="2xl">{game.name}</Heading>
         <HStack justifyContent='space-between'>
           <PlatformIconList
-            platforms={game.parent_platforms.map(({ platform }) => platform)}
+            platforms={game.parent_platforms?.map(({ platform }) => platform)}
           />
           <CriticScore score={game.metacritic} />
         </HStack>
